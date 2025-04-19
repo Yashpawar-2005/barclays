a04c462dd8eab873424cd54b254a9218e34ea367
Shash <shashwat.awate@gmail.com>
2025-04-19 18:53:50 +0530

    changes to text extraction,chunking,structurization

--- python/data_extraction/clean_data.py
+++ python/data_extraction/clean_data.py
@@ -1,41 +1,44 @@
-import sys
-import os
-sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+# import sys
+# import os
+# sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 
-
+from langchain.text_splitter import RecursiveCharacterTextSplitter
 from langdetect import detect,DetectorFactory
 from collections import defaultdict
 import re
-from .ocr import extract_pdf_content
+from data_extraction.ocr import extract_pdf_content
 
 
-def preprocess_data(text):
-    #cleaning data
-    cleaned = []
-    for line in text:
-        clean_line = line
-        clean_line = re.sub(r'^\s+','',line).strip()
-        clean_line = re.sub(r'^[\-=*_.~\s]{3,}|[\-=*_.~\s]{3,}$','',clean_line).strip()
-        clean_line = re.sub(r'\.{3,}', ' ', clean_line).strip()
-        if clean_line and len(clean_line)>10 and (c.isalpha() for c in clean_line):
-            cleaned.append(clean_line)
+def clean_text(line):
+    clean_line = line
+    clean_line = re.sub(r'^\s+','',line).strip()
+    clean_line = re.sub(r'^[\-=*_.~\s]{3,}|[\-=*_.~\s]{3,}$','',clean_line).strip()
+    clean_line = re.sub(r'\.{3,}', ' ', clean_line).strip()
+    return clean_line
 
-    #detecting english sentences for now
+def preprocess_data(doc):
+    
+    splitter = RecursiveCharacterTextSplitter(chunk_size=5000,chunk_overlap=100)
+    content = doc.page_content
+    chunks = splitter.split_text(content)
+
     DetectorFactory.seed = 0
     lang_dict = defaultdict(list)
-    for sentence in cleaned:
+    for chunk in chunks:
+        clean_chunk = clean_text(chunk)
         try:
-            lang = detect(sentence)
+            lang = detect(clean_chunk)
             if lang=='en':
-                lang_dict[lang].append(sentence)
+                lang_dict[lang].append(clean_chunk)
         except:
-            print("line causing issues in detection: ",sentence)
-
+            print("Chunk causing error in detection: ",clean_chunk)
     return lang_dict
 
 
 if __name__=='__main__':
-    pdf_path = 'D:\coding\hack o hire\BAR38GDOIFR001400XD93_F_PC_N.pdf'
+    pdf_path = 'D:\coding\hack o hire\docs\Term Sheet - INE008A08U84.pdf'
     text = extract_pdf_content(pdf_path)
     if text:
-        print(preprocess_data(text))
+        res = preprocess_data(text)
+        print(res)
+        print(len(res['en']),len(res['en'][0]))
--- python/data_extraction/ocr.py
+++ python/data_extraction/ocr.py
@@ -1,12 +1,12 @@
-import sys
-import os
-sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+# import sys
+# import os
+# sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 
+from langchain_core.documents import Document
 
 import cv2
 import pytesseract
-from docx import Document
-import pdfplumber
+import pymupdf
 import pandas as pd
 import platform
 
@@ -15,7 +15,7 @@ if platform.system()=='Windows':
 
 
 def extract_img_content(img_path):
-
+    
     img = cv2.imread(img_path)
     img = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
     text = []
@@ -25,55 +25,45 @@ def extract_img_content(img_path):
     return text
 
 def extract_word_content(word_path):
-    doc = Document(word_path)
-    text = []
-    for para in doc.paragraphs:
-        text.append(para.text)
-    return text
+    result = ""
+    doc = pymupdf.open(word_path)
+    for page in doc:
+        tabs = page.find_tables()
+        if len(tabs.tables)>0:
+            for i in range(len(tabs.tables)):
+                result+= tabs[i].to_markdown()
+        else:
+            result+= page.get_text()
+        pass
+    lc_doc = Document(page_content=result,metadata={"source":"Termsheet docx"})
+    return lc_doc
 
 def extract_pdf_content(pdf_path):
-    text_lines = []
-    images = []
-    table_settings = {
-        "vertical_strategy": "lines",
-        "horizontal_strategy": "lines",
-        "snap_tolerance": 3,
-        "min_words_vertical":2
-    }
-    with pdfplumber.open(pdf_path) as pdf:
-        for page in pdf.pages:
-            table = page.extract_table(table_settings=table_settings)
-            if table:
-                table = pd.DataFrame(table[1::],columns=table[0])
-                table_content = extract_excel_content(dataframe=table)
-                text_lines.extend(content for content in table_content)
-            else:
-                text = page.extract_text()
-                if text:
-                    page_lines = text.splitlines()
-                    text_lines.extend(page_lines)
-                if page.images:
-                    images.extend(page.images)
+    doc  = pymupdf.open(pdf_path)
+    result = ""
+    for page in doc:
+        tabs = page.find_tables()
+        if len(tabs.tables)>0:
+            for i in range(len(tabs.tables)):
+                result+= tabs[i].to_markdown()
+        else:
+            result+= page.get_text()
         pass
-    return text_lines
+    lc_doc = Document(page_content=result,metadata={"source":"Termsheet pdf"})
+    return lc_doc
 
-def extract_excel_content(excel_path = None,dataframe:pd.DataFrame = None):
-    if excel_path:
-        dataframe = pd.read_excel(excel_path)
-    elif dataframe is not None:
-        table_content = []
-        cols = ("Columns: "+ ','.join(str(col) for col in dataframe.columns))
-        rows = ("Rows: "+ '\n'.join([" | ".join(map(str,row)) for row in dataframe.iloc[1:].values]))
-        table_content.append(rows)
-        table_content.append(cols)
-        return table_content
-        pass
+def extract_excel_content(excel_path):
+    result = ""
+    df = pd.read_excel(excel_path)
+    result += df.to_markdown()
+    lc_doc = Document(page_content=result,metadata={"source":"Termsheet excel"})
+    return lc_doc
     
 
 if __name__ == "__main__":
     img_path = "D:\coding\hack o hire\image.png"
-    doc_path = 'D:\coding\hack o hire\PBL report format.docx'
-    pdf_path = 'D:\coding\hack o hire\Term Sheet - INE008A08U84.pdf'
+    doc_path = 'D:\coding\hack o hire\docs\Assignment addressing modes.docx'
+    pdf_path = 'D:\coding\hack o hire\docs\Term Sheet - INE008A08U84.pdf'
     excel_path = 'D:\coding\hack o hire\legal_features.xlsx'
-    txt = extract_pdf_content(pdf_path)
-    print(txt)
+    txt = extract_word_content(doc_path)
+    print(txt.page_content)
